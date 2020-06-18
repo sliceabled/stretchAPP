ced10c9cfa7880d5a74651b9ba92ffc085a1f9d8
slice <maxsinclair1@qq.com>
2020-06-18 11:48:32 +1000

    initialisation

---
+++ StretchAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StretchAPP
+//
+//  Created by Max Sinclair on 17/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
